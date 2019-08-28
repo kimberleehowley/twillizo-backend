9556f852799af3ed443bec11a9b0369b526b12e8
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-08-27 21:18:23 -0700

    Refactored data to live in separate data.json file

--- app.js
+++ app.js
@@ -1,39 +1,35 @@
 const express = require('express');
 const app = express();
 
-// Route to respond with a greeting 
-app.get('/greetings', (req, res) => {
-    res.json({greeting: "Hello World!"})
-})
+// Importing lyrics model for working with data 
+const records = require('./records'); 
 
 // Send a GET request to /lyrics to read all lyrics
+app.get('/lyrics', (req, res) => {
+    const lyrics = records.getQuotes(); 
+    res.json(lyrics); 
+})
+
 // Send a GET request to /lyrics/:id to read (view) a lyric
+app.get('/lyrics/:id', (req, res) => {
+    
+})
+
 // Send a POST request to /lyrics to add a new lyric
+app.post('/lyrics', (req, res) => {
+    
+})
+
 // Send a PUT request to /lyrics/:id to update a lyric
+app.get('/lyrics/:id', (req, res) => {
+    
+})
+
 // Send a DELETE request to /lyrics/:id to delete a lyric
+app.get('/lyrics/:id', (req, res) => {
+    
+})
+
 // Send a GET request to lyrics/lyric/random to read a random lyric 
 
 app.listen(3000, () => console.log('Lizzo API listening on port 3000!'));
-
-// Storing data very locally 
-
-const data = 
-{
-    lyrics: [
-      {
-        "id": 0,
-        "lyric": "Bling bling then I solve 'em that's the goddess in me 💎",
-        "song": "Truth Hurts",
-      },
-      {
-        "id": 1,
-        "lyric": "I know I'm a queen but I don't need no crown 🚫👑",
-        "song": "Soulmate",
-      },
-      {
-        "id": 2,
-        "quote": "The juice ain't worth the squeeze if the juice don't look like this 🍑",
-        "song": "Juice"
-      }
-    ]
-  }
