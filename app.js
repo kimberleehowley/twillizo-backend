c30b8186deb19bad7651844011a435f92b3c9d91
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-08-28 16:56:22 -0700

    Added asyncHandler middleware to remove try and catch statements

--- app.js
+++ app.js
@@ -4,81 +4,96 @@ const app = express();
 // Importing lyrics model for working with data
 const records = require("./records");
 
+// Helper function that wraps another function in try/catch and passes errors to middleware
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
 app.use(express.json());
 
 // Send a GET request to /lyrics to read all lyrics
-app.get("/lyrics", async (req, res) => {
-  try {
+app.get("/lyrics", asyncHandler(async (req, res) => {
     const lyrics = await records.getQuotes();
     res.json(lyrics);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+}));
 
 // Send a GET request to /lyrics/:id to read (view) a lyric
-app.get("/lyrics/:id", async (req, res) => {
-  try {
+app.get("/lyrics/:id", asyncHandler(async (req, res) => {
     const lyric = await records.getQuote(req.params.id);
-    if(lyric) {
-        res.json(lyric);  
+    if (lyric) {
+      res.json(lyric);
     } else {
-        res.status(404).json({message: "Lyric not found!"})
+      res.status(404).json({ message: "Lyric not found!" });
     }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+}));
 
 // Send a POST request to /lyrics to add a new lyric
-app.post("/lyrics", async (req, res) => {
-  try {
+app.post(
+  "/quotes",
+  asyncHandler(async (req, res) => {
     if (req.body.lyric && req.body.song) {
-        const newLyric = await records.createQuote({
-            lyric: req.body.lyric,
-            song: req.body.song
-          });
-          res.status(201).json(newLyric);
+      const newLyric = await records.createQuote({
+        lyric: req.body.lyric,
+        song: req.body.song
+      });
+      res.status(201).json(newLyric);
     } else {
-        res.status(400).json({message: "Lyric and song required!"})
+      res.status(400).json({ message: "Lyric and song required!" });
     }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+  })
+);
 
 // Send a PUT request to /lyrics/:id to update a lyric
-app.put("/lyrics/:id", async (req, res) => {
-    try {
-        const lyric = await records.getQuote(req.params.id);
-        if (lyric) {
-            lyric.lyric = req.body.lyric; 
-            lyric.song = req.body.song; 
+app.put(
+  "/lyrics/:id",
+  asyncHandler(async (req, res) => {
+    const lyric = await records.getQuote(req.params.id);
+    if (lyric) {
+      lyric.lyric = req.body.lyric;
+      lyric.song = req.body.song;
 
-            await records.updateQuote(lyric); 
-            res.status(204).end(); 
-        } else {
-            res.status(404).json({message: "Lyric not found!"})
-        }
-        
-    } catch(err) {
-        res.status(500).json({ message: err.message }); 
+      await records.updateQuote(lyric);
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Lyric not found!" });
     }
-
-});
+  })
+);
 
 // Send a DELETE request to /lyrics/:id to delete a lyric
-app.delete("/lyrics/:id", async (req, res) => {
-    try {
-        const lyric = await records.getQuote(req.params.id);
-        await records.deleteQuote(lyric); 
-        res.status(204).end(); 
-    } catch(err) {
-        res.status(500).json({ message: err.message });
-    }
-});
+app.delete(
+  "/lyrics/:id",
+  asyncHandler(async (req, res, next) => {
+    const lyric = await records.getQuote(req.params.id);
+    await records.deleteQuote(lyric);
+    res.status(204).end();
+  })
+);
 
 // Send a GET request to lyrics/lyric/random to read a random lyric
+
+// Custom middleware
+app.use((req, res, next) => {
+  const err = new Error("Not found.");
+  err.status = 404;
+  next(err);
+});
+
+// Custom error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
 
 // Telling the app where to listen
 app.listen(3000, () => console.log("Lizzo API listening on port 3000!"));
